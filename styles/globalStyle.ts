239e803ccb389a0df5368cccfee380b022455480
wowalswjd <alswjd5491@naver.com>
2024-12-14 04:22:28 +0900

    :bento: 캐릭터 이미지 배열 추가

--- styles/globalStyle.ts
+++ styles/globalStyle.ts
@@ -1,3 +1,10 @@
+import cat from "../assets/images/animals/cat.png";
+import cow from "../assets/images/animals/cow.png";
+import elephant from "../assets/images/animals/elephant.png";
+import monkey from "../assets/images/animals/monkey.png";
+import sheep from "../assets/images/animals/sheep.png";
+import tiger from "../assets/images/animals/tiger.png";
+
 export const globalColor = {
   black: "#09090A",
   darkgrey: "#484646",
@@ -6,3 +13,5 @@ export const globalColor = {
   white: "#FFFFFF",
   pink: "#D18888",
 };
+
+export const characterArr = [cat, cow, elephant, monkey, sheep, tiger];
