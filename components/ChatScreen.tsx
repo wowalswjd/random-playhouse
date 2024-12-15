620861b43f8783c08aa06d49e1473e2f51e11ee5
wowalswjd <alswjd5491@naver.com>
2024-12-16 05:49:36 +0900

    :recycle: chatscreen 반복 코드 리팩토링 및 state 연결

--- components/ChatScreen.tsx
+++ components/ChatScreen.tsx
@@ -6,22 +6,31 @@ import {
 } from "react-native-gesture-handler";
 import AssiMessage from "./AssiMessage";
 import UserMessage from "./UserMessage";
+import { useEffect, useRef } from "react";
+import { ChatType } from "@/app/chatgame";
 
-const ChatScreen = () => {
+const ChatScreen = ({ chatList }: { chatList: ChatType[] }) => {
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  useEffect(() => {
+    // 컴포넌트가 렌더링되면 맨 아래로 스크롤
+    scrollViewRef.current?.scrollToEnd({ animated: true });
+  }, [chatList.length]);
+
   return (
     <Container>
       <GestureHandlerRootView>
-        <ScrollView>
-          <AssiMessage text="uguug" />
-          <UserMessage text="wgdsg" />
-          <AssiMessage text="uguug" />
-          <UserMessage text="wgdsg" />
-          <AssiMessage text="uguug" />
-          <UserMessage text="wgdsg" />
-          <AssiMessage text="uguug" />
-          <UserMessage text="wgdsg" />
-          <AssiMessage text="uguug" />
-          <UserMessage text="wgdsg" />
+        <ScrollView ref={scrollViewRef}>
+          <AssiMessage
+            text={`안녕하세요. 저는 지피티입니다. ${"\n"}고민이 있으신가요?${"\n"}당신의 선택을 도와드릴게요!`}
+          />
+          {chatList.map((message, index) => {
+            return message.role === "user" ? (
+              <UserMessage key={index} text={message.content} />
+            ) : (
+              <AssiMessage key={index} text={message.content} />
+            );
+          })}
         </ScrollView>
       </GestureHandlerRootView>
     </Container>
@@ -34,7 +43,8 @@ const Container = styled.View`
   /* background-color: ${globalColor.darkBeige}; */
   border-width: 1px;
   border-color: ${globalColor.darkBeige};
-  border-radius: 20px;
+  border-radius: 10px;
   width: 100%;
-  height: 100%;
+  height: 96%;
+
 `;
