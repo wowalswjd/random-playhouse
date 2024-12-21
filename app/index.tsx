a52b1a976fe377a3f21f621733fc95c2b7fafeaf
wowalswjd <alswjd5491@naver.com>
2024-12-21 23:29:11 +0900

    :lipstick: 앱 첫 번째 화면 페이지 배경 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,26 @@
 import { router } from "expo-router";
 import { Button, View } from "react-native";
 
+import background from "../assets/images/background.png";
+import styled from "styled-components/native";
+
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button title="dutch-pay" onPress={() => router.navigate("/dutchpay")} />
-      <Button title="ghost-leg" onPress={() => router.navigate("/ghostleg")} />
-      <Button title="chat-game" onPress={() => router.navigate("/chatgame")} />
-
-      {/* 앱 설명 페이지 */}
-    </View>
+    <Background source={background} resizeMode="cover">
+      <Button title="더치페이 계산기" onPress={() => router.navigate("/dutchpay")} />
+      <Button title="사다리타기 게임" onPress={() => router.navigate("/ghostleg")} />
+      <Button title="GPT 의사결정 도우미" onPress={() => router.navigate("/chatgame")} />
+    </Background>
   );
 }
+
+const Background = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+
+  gap: 16px;
+`;
